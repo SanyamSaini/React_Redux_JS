03ca4d44abc9c8ae72752ed736e223a2122c81ab
Sanyam Saini <sanyamsaini51@gmail.com>
2023-01-27 17:08:45 +0530

    Nesting Components.
    Nesting Components.

---
+++ React_Basic_App/myapp/src/Ninjas.js
@@ -0,0 +1,12 @@
+
+function Ninjas() {
+    return(
+        <div className="ninja">
+            <div>Name : Sanyam</div>
+            <div>Age : 27</div>
+            <div>Belt : Black</div>
+        </div>
+    );
+}
+
+export default Ninjas;
